6b8b20f010808c12eb2d738b699f251bd0e7dd03
FabioDamiao <99771144+FabioDamiao@users.noreply.github.com>
2023-04-07 14:44:37 -0300

    Add files via upload

---
+++ level_8/keep_up_the_hoop.js
@@ -0,0 +1,8 @@
+function hoopCount (n) {
+    var resp = 'Keep at it until you get it';
+    var resp2 = 'Great, now move on to tricks';
+     if(n < 10)
+       return resp;
+    else
+      return resp2;
+  }
